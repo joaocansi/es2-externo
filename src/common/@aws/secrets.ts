1ddfc27dfeb1fe84e16cd25b6f5671d23bbc8c56
joaocansi <whitshs2003@gmail.com>
2025-01-01 22:53:28 -0300

    feat: add credential configuration

--- src/common/@aws/secrets.ts
+++ src/common/@aws/secrets.ts
@@ -2,12 +2,14 @@ import {
   SecretsManagerClient,
   GetSecretValueCommand,
 } from '@aws-sdk/client-secrets-manager';
+import { fromIni } from '@aws-sdk/credential-providers';
 import { ConfigService } from '@nestjs/config';
 
 export const fetchSecrets = async (secretName: string) => {
   const configService = new ConfigService();
   const client = new SecretsManagerClient({
     region: configService.get('AWS_REGION'),
+    credentials: fromIni({ profile: 'default' }),
   });
 
   try {
