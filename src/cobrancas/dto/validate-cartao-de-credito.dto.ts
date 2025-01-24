9a5902c5ff3bdb7f583999896288d2294e5b68d1
joaocansi <whitshs2003@gmail.com>
2025-01-23 22:35:46 -0300

    refactor: change patterns from dto

--- src/cobrancas/dto/validate-cartao-de-credito.dto.ts
+++ src/cobrancas/dto/validate-cartao-de-credito.dto.ts
@@ -1,4 +1,4 @@
-import { IsNotEmpty, IsString, Matches } from 'class-validator';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 export default class ValidateCartaoDeCreditoDto {
   @IsString()
@@ -6,15 +6,12 @@ export default class ValidateCartaoDeCreditoDto {
   nomeTitular: string;
 
   @IsString()
-  @Matches(/^\d{16}$/)
   numero: string;
 
   @IsString()
   @IsNotEmpty()
-  @Matches(/^(\d{4})-(\d{2})$/)
   validade: string;
 
   @IsString()
-  @Matches(/^\d{3}$/)
   cvv: string;
 }
